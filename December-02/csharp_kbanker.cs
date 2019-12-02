a270c6b48b95adf59224391d063ee247662abe6e
Kush Banker <kushbanker@gmail.com>
2019-12-01 22:12:23 -0500

    December 2 Solved

---
+++ December-02/csharp_kbanker.cs
@@ -0,0 +1,50 @@
+using System;
+
+class December2
+{
+  bool IsValidNum(long cardNum)
+  {
+    string numString = "" + cardNum;
+    int s1 = 0;
+    int s2 = 0;
+    int[] evens = new int[numString.Length];
+
+    for (int i = 0; i < numString.Length; i++)
+    {
+      if (i % 2 == 0)
+      {
+        s1 += Convert.ToInt32(numString[i].ToString());
+      }
+      else
+      {
+        evens[i] = 2 * Convert.ToInt32(numString[i].ToString());
+      }
+    }
+
+    foreach (int even in evens) {
+      if(even >= 10)
+      {
+        s2 += even % 10;
+        s2 += (int) even / 10;
+      }
+      else
+      {
+        s2 += even;
+      }
+    }
+
+    int sum = s1 + s2;
+
+    if(sum % 10 == 0) { return true; }
+    else { return false; }
+  }
+
+  public static void Main(string[] args)
+  {
+    December2 a = new December2();
+
+    Console.Write("Input: ");
+    long input = Convert.ToInt64(Console.ReadLine());
+    Console.WriteLine(a.IsValidNum(input));
+  }
+}
