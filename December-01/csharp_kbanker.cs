33fc6337ffb1cdb90b21c6e3fc9f5657c5f304ef
Kush Banker <kushbanker@gmail.com>
2019-12-01 20:53:09 -0500

    December 1 Solved

---
+++ December-01/csharp_kbanker.cs
@@ -0,0 +1,39 @@
+using System;
+
+class December1
+{
+  int sevenish_number(int pos)
+  {
+
+    int[] numbers = new int[2 * pos + 1];
+    int currentPower = 1;
+
+    numbers[0] = 1;
+    numbers[1] = 7;
+    numbers[2] = 8;
+
+    for(int i = 3; i < pos; i++)
+    {
+      if(numbers[i] != 0)
+      {
+        continue;
+      }
+
+      currentPower++;
+      numbers[i] = (int) Math.Pow(7, currentPower);
+
+      for(int k = 0; k < i; k++)
+      {
+        numbers[i + 1 + k] = numbers[i] + numbers[k];
+      }
+    }
+
+    return numbers[pos - 1];
+  }
+
+  public static void Main(string[] args)
+  {
+    December1 a = new December1();
+    Console.WriteLine(a.sevenish_number(15));
+  }
+}
