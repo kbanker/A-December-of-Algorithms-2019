8e1aba3716aaaf790f3ed46d0d9ef906a8d381cd
kbanker <kushbanker@gmail.com>
2019-12-07 22:21:42 -0500

    December 07 Solved

---
+++ December-07/python_kbanker.py
@@ -0,0 +1,25 @@
+from collections import deque
+
+q = deque()
+
+n = int(input('Enter N: '))
+
+print('Enter token no, id')
+
+for i in range(n):
+  inp = input()
+  inp = inp.split(',')
+  pair = (int(inp[0]), inp[1].strip())
+  q.append(pair)
+
+k = input('Enter k: ')
+
+k_pair = [i for i in q if k in i]
+
+q.remove(k_pair[0])
+
+print('The order is: ')
+print(k_pair[0])
+
+for i in range(len(q)):
+  print(q.popleft())
